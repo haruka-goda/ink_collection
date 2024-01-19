d974b5be10f0b711cd1f6ca6cbe4ef67c133cec7
haruka goda <haru.goda@gmail.com>
2024-01-19 17:52:58 +0900

    ログインと新規登録のビュー作成、古いページの削除

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,8 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   
-  resources :users, only: %i[new create]  
+  resources :users, only: %i[new create]
 
-  get 'static_pages/login'
-  get 'static_pages/signup'
   get 'static_pages/mypage'
   get 'static_pages/mypageedit'
   get 'static_pages/result'
