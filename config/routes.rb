7b4e4d6107247f9ff6a8c1ded872f75b55d0a2fa
haruka goda <haru.goda@gmail.com>
2024-01-19 17:35:14 +0900

    ユーザーログインのためのコントローラと新規登録のためのコントローラーを作成し、ルーティングを設定する

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+  
+  resources :users, only: %i[new create]  
+
   get 'static_pages/login'
   get 'static_pages/signup'
   get 'static_pages/mypage'
