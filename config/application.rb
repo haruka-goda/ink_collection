d68dd382bbeabd04b3ea038f3444c712a4753780
haruka goda <haru.goda@gmail.com>
2024-01-20 17:10:21 +0900

    タイムゾーンの設定

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,9 @@ module InkCollection
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+    
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
