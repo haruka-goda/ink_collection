8bf4ad8a756753b83754fa7c7b5c3cbea563d410
haruka goda <haru.goda@gmail.com>
2024-01-20 22:59:39 +0900

    エラー表示のためコントローラーの変更、ビューの調整

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,12 +7,12 @@ class UserSessionsController < ApplicationController
       redirect_back_or_to root_path, success: t('.success')
     else
       flash.now[:error] = t('.fail')
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     logout
-    redirect_to root_path, success: t('.success')
+    redirect_to root_path, status: :see_other, success: t('.success')
   end
 end
