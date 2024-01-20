dc3c9e664fc1598d7cf185937587f7b76688439b
haruka goda <haru.goda@gmail.com>
2024-01-20 18:23:59 +0900

    万年筆インク登録のルーティングとコントローラー設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :pens, dependent: :destroy
+	has_many :inks, dependent: :destroy
+
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
