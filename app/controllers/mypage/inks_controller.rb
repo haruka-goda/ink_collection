2043337cbe355039336689d0358239a349b5fdf9
haruka goda <haru.goda@gmail.com>
2024-01-27 10:54:48 +0900

    自分の万年筆とインクページにもページネーション設定

--- app/controllers/mypage/inks_controller.rb
+++ app/controllers/mypage/inks_controller.rb
@@ -1,5 +1,5 @@
 class Mypage::InksController < ApplicationController
   def index
-    @inks = current_user.inks.order(created_at: :desc)
+    @inks = current_user.inks.order(created_at: :desc).page(params[:page])
   end
 end
