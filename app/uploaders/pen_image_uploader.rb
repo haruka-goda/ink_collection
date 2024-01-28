ee3bcb8f3d1f2cc48cba04486229be12476a71ad
haruka goda <haru.goda@gmail.com>
2024-01-29 00:20:17 +0900

    アップローダーの作成

---
+++ app/uploaders/pen_image_uploader.rb
@@ -0,0 +1,15 @@
+class PenImageUploader < CarrierWave::Uploader::Base
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+	def default_url
+    'ink_placeholder.png'
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg png)
+  end
+end
