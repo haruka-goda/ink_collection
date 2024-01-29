bb734ce71816da4d19e446bc07c1946233862bd7
haruka goda <haru.goda@gmail.com>
2024-01-29 17:07:57 +0900

    S3設定

--- app/uploaders/ink_image_uploader.rb
+++ app/uploaders/ink_image_uploader.rb
@@ -1,5 +1,11 @@
 class InkImageUploader < CarrierWave::Uploader::Base
-  storage :file
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
