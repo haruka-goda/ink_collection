d893510cf4082223fd49be7c1a98eb02671afddc
haruka goda <haru.goda@gmail.com>
2024-02-03 16:23:01 +0900

    セレクトボックスの内容をモデルに切り出し

--- app/models/ink.rb
+++ app/models/ink.rb
@@ -5,6 +5,13 @@ class Ink < ApplicationRecord
   validates :name, presence: true, length: { maximum: 40 }
   validates :brand, presence: true
   validates :description, length: { maximum: 140 }, allow_blank: true
+  
+  BRANDS = {
+    '国内ブランド' => ['その他(国内)', 'オリジナルインク', 'ご当地インク', 'セーラー', 'トノアンドリムズ', 'パイロット', 'プラチナ'],
+    '海外ブランド' => ['その他(海外)', 'アウロラ', 'エルバン', 'カヴェコ', 'ダイアミン', 'ペリカン', 'モンブラン', 'ラミー']
+  }.freeze
+  COLORS = ['黒', 'ブルーブラック', '茶', 'グレー', '青', '紫', '緑', 'オレンジ', '赤', 'ピンク', 'その他'].freeze
+  INK_LEVELS = ['40ml以上', '39ml〜11ml', '10ml以下'].freeze
 
   def self.ransackable_attributes(auth_object = nil)
     %w[name brand]
--- app/models/pen.rb
+++ app/models/pen.rb
@@ -6,6 +6,12 @@ class Pen < ApplicationRecord
   validates :brand, presence: true
   validates :description, length: { maximum: 140 }, allow_blank: true
 
+  BRANDS = {
+    '国内ブランド' => ['その他(国内)', 'パイロット', 'セーラー', 'プラチナ'],
+    '海外ブランド' => ['その他(海外)', 'アウロラ', 'カヴェコ', 'カランダッシュ', 'デルタ', 'パーカー', 'ビスコンティ', 'ペリカン', 'モンブラン', 'モンテグラッパ', 'ラミー']
+  }.freeze
+  NIB_SIZES = ['BB', 'B', 'MB', 'M', 'F', 'EF'].freeze
+
   def self.ransackable_attributes(auth_object = nil)
     %w[name brand]
   end
